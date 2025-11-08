8610218a7bb7bdde5318a1befe09a75e40214a17
Shivam24S <shivamshrivastav8055@gmail.com>
2025-11-08 15:54:58 +0530

    variable

---
+++ JS/1.variable.js
@@ -0,0 +1,36 @@
+// var
+
+// function sayName() {
+//   var name;
+//   //   var name = "john";
+
+//   name = "alice";
+
+//   console.log(name);
+// }
+
+// sayName();
+
+function greeting() {
+  let message = "good morning";
+
+  if (message) {
+    let reply = "very good morning";
+
+    reply = "good afternoon";
+
+    console.log(reply);
+    console.log(message);
+  }
+  //   console.log(reply);
+}
+
+greeting();
+
+// person
+
+const number = 24;
+
+number = 50;
+
+console.log(number);

9e27bd91024ea7da07f770e30d1c12e4bd3b84b1
Shivam24S <shivamshrivastav8055@gmail.com>
2025-11-15 15:27:09 +0530

    js code added

---
+++ JS/2.operators.js
@@ -0,0 +1,38 @@
+// arithmetic operator
+
+const a = 10;
+
+const b = 5;
+
+console.log("addition", a + b);
+console.log("subtraction", a - b);
+console.log("multiplication", a * b);
+console.log("division", a / b);
+console.log("module", a % b);
+
+// assignment operator
+
+let c = 20;
+
+c += 20;
+c -= 20;
+c *= 20;
+c /= 20;
+c %= 20;
+
+console.log(c);
+
+let num = 20;
+
+console.log("previous", num);
+
+++num;
+console.log("after", num);
+
+let num2 = 30;
+
+console.log(num2);
+
+num2++;
+
+console.log(num2);
---
+++ JS/5.ternary.js
@@ -0,0 +1,9 @@
+let age = 50;
+
+// if (age < 18) {
+//   console.log("you can't drive vehicle");
+// } else {
+//   console.log("you can drive vehicle");
+// }
+
+age > 18 ? console.log("you can drive vehicle") : "you can't drive vehicle";
